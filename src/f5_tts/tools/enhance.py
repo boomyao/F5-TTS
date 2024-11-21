18e204e244baf93ad309ba1cc6a15e46b1d98a09
boomyao <boomyao@outlook.com>
2024-11-21 20:58:15 +0800

    fix: enhance dtype

--- src/f5_tts/tools/enhance.py
+++ src/f5_tts/tools/enhance.py
@@ -4,6 +4,7 @@ import torch
 def enhance_audio(wav: torch.Tensor, sr: int):
     try:
         wav = wav.mean(dim=0)
+        wav = wav.to(torch.float32)
         wav, new_sr = enhance(wav, sr, device="cuda")
         wav = wav.cpu()
         torch.cuda.empty_cache()
